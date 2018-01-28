c66c2685aa4446a5ec8d5c2d4889efdc8086dafc
Ngr <youkuikunda@gmail.com>
2018-01-28 23:15:26 +0545

    configurationn added in configuration/index.js and token is generated using jwt

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,15 @@
 const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET } = require('../configuration');
+
+signToken = user => {
+	return JWT.sign({
+			iss: 'can be name or name of out server or app',
+			sub: user.id,
+			iat: new Date().getTime(),// current time
+			exp: new Date().setDate(new Date().getDate()+1) //current time + 1 day ahead
+		}, JWT_SECRET);
+};
 
 module.exports = {
 	signUp: async (req, res, next) => {
@@ -11,11 +21,10 @@ module.exports = {
 		// Check if there is a user with the same email
 		// const foundUser = User({email: email});
 		// ES6 : if key and value reference have same name then we can do 
-		const foundUser = User({email});
+		const foundUser = await User.findOne({ email });
 		if( foundUser ){
 			return res.status(403).json({error: 'Email already in use'});// 403 : forbidden
 		}
-
 
 		// Create a new user
 		// const newUser = new User({
@@ -27,14 +36,18 @@ module.exports = {
 		await newUser.save();
 
 		// respond with token
-		const token = JWT.sign({
+		/*const token = JWT.sign({
 			iss: 'can be name or name of out server or app',
 			sub: newUser.id,
 			iat: new Date().getTime(),// current time
 			exp: new Date().setDate(new Date().getDate()+1) //current time + 1 day ahead
-		}, 'apiauthentication secret');
+		}, 'apiauthentication secret');*/
 
 
+		// generate token
+		const token = signToken(newUser);
+
+		// respond with token
 		res.status(200).json({ token }); 
 	},
 
